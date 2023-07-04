b800cb35bde0f2c3163c8d638f978455220e6c23
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-07-04 09:42:01 -0300

    BBVAFSWT2-17 | Crear modelo y repositorio de Users

---
+++ wallet/src/main/java/com/bbva/wallet/entities/User.java
@@ -0,0 +1,50 @@
+package com.bbva.wallet.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    String firstName;
+
+    @Column(nullable = false)
+    String lastName;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "role_id")
+    private Role roleId;
+
+    @CreationTimestamp
+    LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    LocalDateTime updatedDate;
+
+    @JsonIgnore
+    boolean active;
+}
+
