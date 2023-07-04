ecbfda0bc03f4018f9920af03120804f90b36894
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-07-04 19:11:47 -0300

    Crea controller con ruta
    Inyecto el service y después defino el endpoint.

---
+++ wallet/src/main/java/com/bbva/wallet/controllers/AccountController.java
@@ -0,0 +1,28 @@
+package com.bbva.wallet.controllers;
+
+import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.services.AccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("{userId}")
+    public List<Account> findAccountsByUser (@PathVariable Long userId){
+        return accountService.findAccountsByUser(userId);
+    }
+
+
+}
