f78e8a147e541c5c1104457bd0a1fb8acdfe776d
simonnava <simonnava2112@gmail.com>
2023-07-05 10:12:14 -0300

    BBVAFSW3T2-1 Cree modelo y repositorio de FixedTermDeposits

---
+++ wallet/src/main/java/com/bbva/wallet/repositories/FixedTermDepositRepository.java
@@ -0,0 +1,8 @@
+package com.bbva.wallet.repositories;
+
+import com.bbva.wallet.entities.FixedTermDeposit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FixedTermDepositRepository  extends JpaRepository<FixedTermDeposit, Long> {
+
+}
