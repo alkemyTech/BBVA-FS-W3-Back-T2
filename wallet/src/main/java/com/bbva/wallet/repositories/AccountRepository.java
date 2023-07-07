bd5f1d8a6eca02694659337becec6b6eecd9fd60
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-07-07 11:07:22 -0300

    Acomoda tipo de Dato del id en AccountRepository

--- wallet/src/main/java/com/bbva/wallet/repositories/AccountRepository.java
+++ wallet/src/main/java/com/bbva/wallet/repositories/AccountRepository.java
@@ -3,5 +3,5 @@ package com.bbva.wallet.repositories;
 import com.bbva.wallet.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, String> {
 }
