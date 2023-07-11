fdd600749ad5d2844e23c9cbe23b70b559e4a8c4
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-07-11 09:39:23 -0300

    BBVAFSW3T2-51 | Crear Seed Data de Usuarios

---
+++ wallet/src/main/java/com/bbva/wallet/seeders/RoleSeeder.java
@@ -0,0 +1,31 @@
+package com.bbva.wallet.seeders;
+
+import com.bbva.wallet.entities.Role;
+import com.bbva.wallet.enums.RoleName;
+import com.bbva.wallet.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RoleSeeder implements CommandLineRunner {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadRoleData();
+    }
+
+    private void loadRoleData() {
+        if (roleRepository.count() == 0) {
+            Role userRole = Role.builder().name(RoleName.USER).description("User").build();
+            Role adminRole = Role.builder().name(RoleName.ADMIN).description("Admin").build();
+            roleRepository.saveAll(List.of(userRole, adminRole));
+        }
+        System.out.println(roleRepository.count());
+    }
+
+}
