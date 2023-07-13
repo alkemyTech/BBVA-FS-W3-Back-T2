8b7a58d67b314b5c01640e98ba3c84eb2db8de65
Valentino Veralli <valentinoveralli@gmail.com>
2023-07-13 13:33:18 -0300

    BBVAFSW3T2-40 Endpoint para obtener el detalle de una transacción

--- wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
@@ -2,7 +2,6 @@ package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.entities.Transaction;
 import com.bbva.wallet.services.TransactionService;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
--- wallet/src/main/java/com/bbva/wallet/services/TransactionService.java
+++ wallet/src/main/java/com/bbva/wallet/services/TransactionService.java
@@ -3,7 +3,10 @@ package com.bbva.wallet.services;
 import com.bbva.wallet.entities.Transaction;
 import com.bbva.wallet.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAuthority;
 
 @Service
 public class TransactionService {
