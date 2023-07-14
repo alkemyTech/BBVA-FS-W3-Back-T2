af0e2277f119dd1df783e01b70fce904ee81eec3
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-07-13 22:35:28 -0300

    Suma método Page<User> findAll

--- wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
+++ wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.bbva.wallet.repositories;
 import com.bbva.wallet.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -7,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Page<User> findAll(Pageable pageable);
+
 }
 
