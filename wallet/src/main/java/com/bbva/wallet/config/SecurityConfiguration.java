79dcb37cd506ce311d0133f4ebd882bf85fb4393
Valentino Veralli <valentinoveralli@gmail.com>
2023-07-24 23:07:23 -0300

    update

--- wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
+++ wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
