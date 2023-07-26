6117f8bc265c959dcb96d52a1b2b2fdb27909705
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-07-26 01:12:43 -0300

    Agregada autorización exclusiva para admins al endpoint para obtener el listado de cuentas paginadas

--- wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
+++ wallet/src/main/java/com/bbva/wallet/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.bbva.wallet.services.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -82,6 +83,7 @@ public class SecurityConfiguration {
                                         "/v3/api-docs/**", "/error")
                                 .permitAll()
                                 .requestMatchers("/users").hasAuthority(RoleName.ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/accounts").hasAuthority(RoleName.ADMIN.name())
                                 .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
