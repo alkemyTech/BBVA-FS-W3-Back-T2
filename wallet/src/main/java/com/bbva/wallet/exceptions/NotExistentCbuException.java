b253574ea227055d138bc467df2b22b65d818902
simonnava <simonnava2112@gmail.com>
2023-08-01 13:34:10 -0300

    arreglando problema en NotExistentCbuException

--- wallet/src/main/java/com/bbva/wallet/exceptions/NotExistentCbuException.java
+++ wallet/src/main/java/com/bbva/wallet/exceptions/NotExistentCbuException.java
@@ -1,15 +1,12 @@
 package com.bbva.wallet.exceptions;
 
-import com.bbva.wallet.entities.User;
 
 public class NotExistentCbuException extends RuntimeException{
     private final String cbu;
-    private User firstName;
-
     public NotExistentCbuException(String message, String cbu) {
         super(message);
         this.cbu = cbu;
-        this.firstName = firstName;
+
     }
 
     public String getCbu() { return cbu;}
